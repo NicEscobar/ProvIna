506da9fe1f997b4c3bb925ef0ea0f350c063924f
NicEscobar <nalves@neoenergia.com>
2021-06-10 20:38:45 -0300

    com  mobile

--- backend/src/Servidor.js
+++ backend/src/Servidor.js
@@ -3,16 +3,18 @@ const routes = require('./router');
 const cors = require('cors');
 const morgan = require('morgan');
 
+/*
 const corsOptions ={
     
     origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
-}
+}*/
 
 const app = express();
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({limit: '500mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 //app.use(morgan("dev"));
